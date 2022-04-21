530835500e0108361ef9467d268c9e7135fce468
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2022-04-21 14:02:43 -0700

    Delete spring-demo-one directory

--- spring-demo-one/src/com/joshlikestocode/springdemo/Coach.java
+++
@@ -1,8 +0,0 @@
-package com.joshlikestocode.springdemo;
-
-public interface Coach {
-	// gives daily work outs
-	public String getDailyWorkOut();
-	// gives daily fortunes
-	public String getDailyFortune();
-}
--- spring-demo-one/src/com/joshlikestocode/springdemo/FortuneService.java
+++
@@ -1,6 +0,0 @@
-package com.joshlikestocode.springdemo;
-
-public interface FortuneService {
-
-	public String getFortune();
-}
